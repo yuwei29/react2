5500904a48b3d3ee376476315135d8e8c960cad7
yuwei29 <121273951+yuwei29@users.noreply.github.com>
2023-02-03 15:40:57 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'https://cdn.jsdelivr.net/npm/react-dom@18.2.0/+esm'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, World!
+        </p>
+      </header>
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById('app'));
